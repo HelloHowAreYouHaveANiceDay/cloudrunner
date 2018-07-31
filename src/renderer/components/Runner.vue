d3b2f6396ab7d8066108e803a70e4125a3cbb8e0
Libo Li <liboli.lilibo@gmail.com>
2018-07-31 09:34:13 -0400

    initial scaffolding

---
+++ src/renderer/components/Runner.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="wrapper">
+    <main>
+      <div class="left-side">
+        <span class="title">
+          this is a vue project
+        </span>
+        <!-- <system-information></system-information> -->
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+// import SystemInformation from './LandingPage/SystemInformation';
+
+export default {
+  name: 'landing-page',
+  components: {
+    // SystemInformation
+  },
+  methods: {},
+};
+</script>
+
+<style>
+</style>
