4a5ee92e6a3dac99c6fe78b6bb3c3456860f6ace
Libo Li <liboli.lilibo@gmail.com>
2018-08-02 14:09:24 -0400

    figuring out components

--- src/renderer/components/Editor/Viewport.vue
+++ src/renderer/components/Editor/Viewport.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
     <p>
-    look at me. I'm a viewport
+    look at me. I'm a viewport {{width}} x {{height}}
     </p>
+    <button>
+      addRow
+    </button>
+    <button>
+      addColumn
+    </button>
     <svg>
 
     </svg>
@@ -18,18 +24,44 @@ export default {
     window.addEventListener('resize', this.onResize);
     this.onResize();
   },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.onResize);
+  },
   data(){
     return {
       width: 0,
       height: 0,
       blocks: [],
+      grid:{
+        rows:0,
+        cols:0
+      },
       edges: [],
+      scale: {
+        x: null,
+        y: null,
+      }
     }
   },
   methods: {
     onResize() {
       this.width = this.$el.offsetWidth;
       this.height = this.$el.offsetHeight;
+    },
+    update() {
+      this.scale.x.range([0, this.width]);
+      this.scale.x.range([this.height, 0]);
+    },
+    initialize(){
+      this.scale.x = d3.scaleLinear()
+        .domain([0,1])
+        .range([0, this.width]);
+      this.scale.y = d3.scaleLinear()
+        .domain([0,1])
+        .range([this.height, 0]);
+    },
+    addRow(){
+
     }
   }
 }
