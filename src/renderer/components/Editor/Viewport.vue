149fb0d772d4d30acd0a88a83a77cd1658752e0f
Libo Li <liboli.lilibo@gmail.com>
2018-08-02 13:03:07 -0400

    added viewport

---
+++ src/renderer/components/Editor/Viewport.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <p>
+    look at me. I'm a viewport
+    </p>
+    <svg>
+
+    </svg>
+  </div>
+</template>
+
+<script>
+import * as d3 from 'd3';
+
+export default {
+  name: 'Viewport',
+  mounted() {
+    window.addEventListener('resize', this.onResize);
+    this.onResize();
+  },
+  data(){
+    return {
+      width: 0,
+      height: 0,
+      blocks: [],
+      edges: [],
+    }
+  },
+  methods: {
+    onResize() {
+      this.width = this.$el.offsetWidth;
+      this.height = this.$el.offsetHeight;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/renderer/components/Runner.vue
+++ src/renderer/components/Runner.vue
@@ -3,17 +3,22 @@
     <div class="content">
         <channels></channels>
     </div>
+    <div>
+        <viewport></viewport>
+    </div>
   </div>
 </template>
 
 <script>
 // import SystemInformation from './LandingPage/SystemInformation';
 import Channels from './Channels/Channels';
+import Viewport from './Editor/Viewport';
 
 export default {
   name: 'landing-page',
   components: {
     Channels,
+    Viewport,
     // SystemInformation
   },
   methods: {},
