a8107f94bcb157e538d9233f3e345c9dec713dba
Libo Li <liboli.lilibo@gmail.com>
2018-08-02 16:21:55 -0400

    hold

--- src/renderer/components/Editor/Viewport.vue
+++ src/renderer/components/Editor/Viewport.vue
@@ -10,37 +10,57 @@
       addColumn
     </button>
     <svg>
-
+      <line v-for="(row, index) in gridLines.rows" 
+        :key="index"
+        :x1="row" 
+        :x2="row" 
+        y1="0" 
+        :y2="height"></line>
     </svg>
   </div>
 </template>
 
 <script>
-import * as d3 from 'd3';
+import * as d3 from "d3";
 
 export default {
-  name: 'Viewport',
+  name: "Viewport",
   mounted() {
-    window.addEventListener('resize', this.onResize);
+    this.initialize();
+    window.addEventListener("resize", this.onResize);
     this.onResize();
   },
   beforeDestroy() {
-    window.removeEventListener('resize', this.onResize);
+    window.removeEventListener("resize", this.onResize);
   },
-  data(){
+  data() {
     return {
       width: 0,
       height: 0,
       blocks: [],
-      grid:{
-        rows:0,
-        cols:0
+      grid: {
+        rows: 4,
+        cols: 0
       },
       edges: [],
       scale: {
         x: null,
-        y: null,
+        y: null
       }
+    };
+  },
+  computed: {
+    gridLines() {
+      // rows
+      const rows = [];
+      for (let i = 1; i < this.grid.rows; i++) {
+        const x = 1 / (this.grid.rows + 1) * i;
+        rows.push(this.scale.x(x));
+      }
+      // cols
+      return {
+        rows
+      };
     }
   },
   methods: {
@@ -52,21 +72,20 @@ export default {
       this.scale.x.range([0, this.width]);
       this.scale.x.range([this.height, 0]);
     },
-    initialize(){
-      this.scale.x = d3.scaleLinear()
-        .domain([0,1])
+    initialize() {
+      this.scale.x = d3
+        .scaleLinear()
+        .domain([0, 1])
         .range([0, this.width]);
-      this.scale.y = d3.scaleLinear()
-        .domain([0,1])
+      this.scale.y = d3
+        .scaleLinear()
+        .domain([0, 1])
         .range([this.height, 0]);
     },
-    addRow(){
-
-    }
+    addRow() {}
   }
-}
+};
 </script>
 
 <style scoped>
-
 </style>
